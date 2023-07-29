1c607e6bcf85ffd981714a09190a57cbdaaa620e
prashant9420 <prashantpal2468@gmail.com>
2023-07-29 12:10:18 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import logo from "./logo.svg";
 import "./App.css";
-import react, { useState } from "react";
+import react, { useState,useEffect} from "react";
 
 function App() {
   const [file, setFile] = useState();
   const [img,setImg]=useState(null);
+  const [allImgs,setAllImgs]=useState([]);
+  const [selectedImg,setSelectedImg]=useState();
   const handleClick =  async () => {
     const formData=new FormData();
     formData.append("file",file);
-    console.log(formData.file);
     const resp = await  fetch("http://localhost:5000/upload", {
       method: "POST",
       body:formData,
     }); 
-    console.log(resp)
     const data = await resp.json();
-    console.log(data.image);
+  
     setImg(data.image);
 
   };
+  useEffect(() => {
+    getAllImages();
+  }, []);
+  const getAllImages=async()=>{
+    const resp=await fetch("http://localhost:5000/getAllImgs")
+    const data=await resp.json();
+    setAllImgs(data.files);
+
+  }
+
+  const handleSelectChange = (event) => {
+    setSelectedImg(event.target.value);
+  };
+
+
   return (
     <div className="App">
       <div>
-        {(img)?<img src={`http://localhost:5000/${img.path}`}/>:null}
+        <select onChange={handleSelectChange}>
+          <option>None</option>
+          {
+            allImgs.map((img)=>{
+              return <option key={img}>{img}</option>
+            })
+          }
+        </select>
+      </div>
+      <div>
+        {(selectedImg)?<img src={`http://localhost:5000/uploads/${selectedImg}`}/>:null}
       </div>
       <input type="file" name="uploaded_img" onChange={(e)=>{setFile(e.target.files[0]);console.log(e.target.files[0])}}/>
       <button onClick={() => handleClick()}>submit</button>
